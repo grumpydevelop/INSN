908f0446f5fda3b1fbbbf81ee9a7bb6d21d54e90
grumpydevelop <simsim0331@gmail.com>
2017-12-10 01:07:49 +0300

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Insanity");
+const std::string CLIENT_NAME("Cryptogenerator");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   ""
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("Insanity");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "Membrane-04"
+#    define GIT_COMMIT_ID "Cryptogenerator"
 #    define GIT_COMMIT_DATE "May 24, 2017" //$Format:%cD
 #endif
 
